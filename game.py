1cfc30d674c5d0a7fb9017b8a07160ff1e1465e2
xavier.niquet <niquet_xavier2002@yahoo.fr>
2020-04-16 10:43:23 +0200

    add score more options

--- game.py
+++ game.py
@@ -1,25 +1,27 @@
 
 from random import choice
-status=["rock","paper","scissors"]
+DEFAULT_OPTIONS=["rock","paper","scissors"]
+
+status=DEFAULT_OPTIONS
 
 def validate(**args):
     def decorator(f):
         def wrap():
             choice=""
-            valid_options=[args["options"] ] + args[status]
+            valid_options=args["options"] + status
             while not (choice in valid_options):
                 choice=f()
                 if not (choice in valid_options):
                     print(args["errors"])
 
-            return "" if choice=="!exit" else choice
+            return choice[1:] if choice.startswith("!") else choice
 
         return wrap
 
     return decorator
 
 
-@validate(options="!exit",errors="Invalid input",status=status)
+@validate(options=["!exit","!rating"],errors="Invalid input")
 def player_choice():
     return input()
 
@@ -29,14 +31,35 @@ def computer_choice():
 def id_status(state):
     return status.index(state)
 
+def check(computer,player):
+    """
+    determine if player is beating or defaiting
+    rule game
+       options= [  left ] +[ choice_computer] + [rigth]
+        player defeating if he is  in second half of [right]+[left]
+    """
+    size=len(status)
+    id=id_status(computer)
+    reorder=status[id+1:]+status[:id]
+    defeating=reorder[size//2:]
+    return player in defeating
+
+def check_default(computer,player):
+    """
+    classical game rock paper scissors
+    determine if player is defeating or beating
+
+    """
+    return (id_status(player) + 1) % 3 == id_status(computer)
 
 def compare(computer,player):
-    if player == computer:
+    if player==computer:
         return ("Draw",computer)
-    elif (id_status(player) + 1) % 3 == id_status(computer):
-        return ("Lose",computer)
+    elif check(computer,player):
+          return ("Lose",computer)
     else:
         return ("Win",computer)
+
 
 def render_state(state,choice):
     if state=="Draw":
@@ -50,18 +73,52 @@ def render_state(state,choice):
 def play():
     return (player_choice(),computer_choice())
 
+def update_score(score,status):
+    if status == "Draw":
+        return score + 50
+    elif status == "Win":
+        return score + 100
+    else:
+        return score
 
-def game():
-
+def game(score_start):
+    score=score_start
     while True:
         player,computer = play()
-        if not player:
+        if  player=="exit":
             print("Bye!")
             break
-        result = compare(computer,player)
-        render_state(*result)
+        elif player=="rating":
+            print(f"Your rating: {score}")
+        else:
+            game_result,choice_computer = compare(computer,player)
+            score=update_score(score,game_result)
+            render_state(game_result,choice_computer)
+
+def find_player(name_player):
+     f_score=open("rating.txt")
+     for line in f_score.readlines():
+         name, score = line.split(" ")
+         if name==name_player:
+             return int(score)
+     else:
+         return 0
+def get_options():
+    options=input()
+    if options=="":
+        return DEFAULT_OPTIONS
+    else:
+        return options.split(",")
+
+def start_game():
+    global status
+    name=input("Enter your name: ")
+    print(f"Hello, {name}")
+    score=find_player(name)
+    status=get_options()
+    print("Okay, let's start")
+    game(score)
 
 
-
-game()
+start_game()
 
